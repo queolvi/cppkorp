a0b72960893a3f51867d51ea23c99ecb9f8a836a
thekorp <korpachyovdmitriy@gmail.com>
2024-10-26 00:07:55 +0300

    // commit :)

--- CHALLENGES/CH-24/main.cpp
+++ CHALLENGES/CH-24/main.cpp
@@ -1,35 +1,7 @@
 #include <iostream>
 #include <algorithm>
 
-int minPairProductSum(int* arr, int size) { 
-    if (size < 2) {
-        return 0; 
-    }
-
-    // Сортируем массив по возрастанию
-    std::sort(arr, arr + size);
-
-    int minSum = 0;
-    int i = 0; // Индекс для минимальных элементов
-    int j = size - 1; // Индекс для максимальных элементов
-
-    // Изменяем условие в цикле:
-    // i < j - 1, так как мы берем пару элементов за один раз
-    while (i < j - 1) { 
-        minSum += arr[i] * arr[j] + arr[i + 1] * arr[j - 1]; // Суммируем произведения пар
-        i += 2; // Переходим к следующей паре минимальных
-        j -= 2; // Переходим к следующей паре максимальных
-    }
-
-    
-    if (i == j - 1) { 
-        minSum += arr[i + 1] * arr[j - 1]; 
-    } else if (size == 2) {
-        minSum += arr[0] * arr[1];
-    }
-
-    return minSum;
-}
+int minPairProductSum(int* arr, int size);
 
 int main() {
     int size = 0;
@@ -52,4 +24,42 @@ int main() {
 
     delete[] arr;
     return 0;
+}
+
+ 
+int minPairProductSum(int* arr, int size) { 
+    if (size < 2) {
+        return 0; 
+    }
+
+    // Сортируем массив по возрастанию
+    std::sort(arr, arr + size);
+
+    int minSum = 0;
+    int i = 0; // Индекс для минимальных элементов
+    int j = size - 1; // Индекс для максимальных элементов
+
+    // Если количество элементов нечетное, то умножаем второй с конца и предпоследний элементы
+    if (size % 2 != 0) {
+        minSum += arr[i] * arr[j];
+        i += 1; // Переходим к следующему минимальному
+        j -= 1; // Переходим к следующему максимальному
+    }
+
+    
+    // i < j - 1, так как мы берем пару элементов за один раз
+    while (i < j - 1) { 
+        minSum += arr[i] * arr[j] + arr[i + 1] * arr[j - 1]; // Суммируем произведения пар
+        i += 2; // Переходим к следующей паре минимальных
+        j -= 2; // Переходим к следующей паре максимальных
+    }
+
+    // Если в массиве остался один элемент после цикла while
+    if (i == j) {
+        minSum += arr[i];
+    } else if (size == 2) {
+        minSum += arr[0] * arr[1];
+    }
+
+    return minSum;
 }
