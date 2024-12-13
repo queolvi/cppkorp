55a8091221e3e50e3202452e82edd9c713af2a4d
queolvi <korpachyovdmitriy@gmail.com>
2024-12-13 11:20:36 +0300

    comm

--- ASM/3.cpp
+++ ASM/3.cpp
@@ -1,32 +1,62 @@
 #include<iostream>
 
-long long foo(long long n) {
+long long foo(long long eax) {
 
 long long unsigned rdi;
 long long unsigned rbp_24 = rdi;
-
+int rbp_4;
+long long rdx;
+long long unsigned rcx;
 long long unsigned rax = rdi;
 rax += 9;
 
 if(rax = 18) {
-	int rbp_4 = 1;
+	 rbp_4 = 1;
        if(rbp_24 != 0) {
-       
+	rcx = rbp_24;
+        rdx = std::abs(7378697629483820647);
+        rax = rcx;
+        eax *= rdx;
+        rdx >>= 2;
+        rax = rcx;
+        rax >>= 63;
+        rdx -= rax;
+        rax = rdx;
+        rax <<= 2;
+        rax += rdx;
+        rax += rax;
+        rcx -= rax;
+        rdx = rcx;
+        eax = rbp_4;
+        eax *= rdx;
+        rbp_4 = eax;
+        rcx = rbp_24;
+        rdx = std::abs(7378697629483820647);
+        rax = rcx;
+        eax *= rdx;
+        rax = rdx;
+        rax >>= 2;
+        rcx >>= 63;
+        rdx = rcx;
+        rax -= rdx;
+        return rbp_24 = rax;
+
+
        } else {
        
-	long long unsigned rcx = rbp_24;
-	long long rdx = std::abs(7378697629483820647);
-	rax = rcx;
-	//17 строка	
-
+	return eax = rbp_4;
 
        }	
 }
-
+return -1;
 }
 
 int main() {
 
+
+	for(int i = -100; i < 100; ++i) {
+		std::cout << i  << "\t" << foo(i) << "\n";
+}
 
 	return 0;
 }
